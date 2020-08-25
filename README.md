b1b21316b5afa27ba4a443f3a78c18cb9420ce6b
Israel Hudson <israelhudson@gmail.com>
2020-08-25 11:25:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_clean_solid_tdd
+ Prática para aprendizado de TDD, SOLID e Design Patterns.
---
+++ android/app/src/main/kotlin/br/com/ihudtecnologia/flutter_clean_solid_tdd/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.ihudtecnologia.flutter_clean_solid_tdd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
