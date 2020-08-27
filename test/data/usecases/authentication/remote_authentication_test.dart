33a47e81cb2fedde4feecb6042bf1abdccdfa5ab
Israel Hudson <israelhudson@gmail.com>
2020-08-26 22:29:06 -0300

    ensure RemoteAuthentication throws on 500

--- test/data/usecases/authentication/remote_authentication_test.dart
+++ test/data/usecases/authentication/remote_authentication_test.dart
@@ -52,4 +52,13 @@ void main(){
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw UnexpectedError of HttpClient returns 500', () async {
+    when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
+      .thenThrow(HttpError.serverError);
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
